93e53dac376298776f8f46f3bde94a362d8ff85e
Mathew Ritchie <ritchmat89@gmail.com>
2025-05-19 20:51:05 +0200

    major changes to AboutMe component
    removed picture of me and added a background image. Also redid the About me text

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -6,24 +6,38 @@ export default function AboutMe() {
   return (
     <section className="about-me-div" id="about-me-div">
       <div className="about-spacer" id="about-spacer"></div>
-      <h1 className="about-me-heading">About me</h1>
-      <div className="about-me-grid">
-        <img
+      <div className="about-me-paragraph-div">
+        <h1 className="about-me-heading">About me</h1>
+
+        {/* <img
           className="about-me-img"
           src={boat}
           alt="Picture of Mathew Ritchie on a boat and table mountian in the background"
-        />
+        /> */}
+
         <p className="about-me-p">
-          I live in Cape Town, and I am relatively new to the web development world, but I already
-          love it!
+          Currently based in the vibrant community of Hout Bay, Cape Town, I'm an easy-going and
+          friendly individual who appreciates a balanced approach to life. My inherent drive and
+          meticulous nature naturally align with my passion for web development, where I find
+          immense satisfaction in creative problem-solving and thinking innovatively.
           <br />
-          <br />I have just finished my Software Development course at CodeSpace Academy, which has
-          given me a great foundation for what is hopefully to come. I have a good understanding of
-          HTML, CSS and most importantly JavaScript and React.
           <br />
-          <br />I am hoping to start a new and exciting career in the Web Development world. I love
-          being creative and problem solving, while creating new things. I am looking for
-          opportunities to learn, grow, and hopefully contribute to something exciting.
+          The dynamic nature of front-end development truly excites me, particularly the art of
+          crafting aesthetically pleasing, user-friendly, and responsive interfaces through
+          thoughtful styling and component design. I'm particularly enthusiastic about leveraging
+          JavaScript and React to build dynamic applications with virtually limitless possibilities,
+          focusing on creating intuitive and engaging user experiences.
+          <br />
+          <br />
+          My formal training at CodeSpace Academy, where I graduated with distinction from the
+          Software Development course, has provided me with a strong technical foundation. I possess
+          practical experience with Git and Github, VSCode, HTML, CSS, CSS Tailwinds, JavaScript,
+          Typescript, and React.
+          <br />
+          <br />I am currently seeking a permanent position within a collaborative environment where
+          I can continue to expand my coding expertise and contribute positively to team and
+          organizational objectives. I am eager to engage with challenging projects and further
+          develop my skills in a supportive and growth-oriented setting.
         </p>
       </div>
     </section>
