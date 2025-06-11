1c7a992da5d87bb7aca7726321fe257c3dfe9d34
Mathew Ritchie <ritchmat89@gmail.com>
2025-06-11 15:58:19 +0200

    FakeStorePage.jsx
    updated jsx and CSS. Also added text discription related to the project

--- src/pages/FakeStorePage.jsx
+++ src/pages/FakeStorePage.jsx
@@ -32,8 +32,10 @@ export default function FakeStorePage() {
             </div>
           ))}
         </div>
-        <button onClick={handlePrev}>Previous</button>
-        <button onClick={handleNext}>Next</button>
+        <div className="carousel-btn-div">
+          <button onClick={handlePrev}>Previous</button>
+          <button onClick={handleNext}>Next</button>
+        </div>
       </div>
       <div>
         <p className="project-technologies">| JAVASCRIPT | REACT | ZUSTAND | FIREBASE</p>
@@ -44,7 +46,7 @@ export default function FakeStorePage() {
           target="_blank"
           className="project-page-btns"
         >
-          GitHub Repository
+          Github Repository
         </a>
         <a
           href="https://mathews-fake-store.netlify.app/"
@@ -56,31 +58,30 @@ export default function FakeStorePage() {
       </div>
       <div>
         <p className="project-description">
-          This project was the culmination of my Web Development course at CodeSpace Academy. This
-          was an opportunity to put all that I had learnt during my time at CodeSpace into a single
-          application.
+          This project showcases a dynamic web store built with a modern technology stack, focusing
+          on a seamless user experience. The front-end is powered by React, providing a robust and
+          component-based architecture for efficient development and maintainability. To manage the
+          application's state effectively, I integrated Zustand, a lightweight and flexible state
+          management solution that ensures predictable data flow throughout the application.
+          <br></br>
+          <br></br>A core feature of this store is its user authentication system. I leveraged
+          Firebase to handle user registration and secure storage of user credentials, enabling
+          users to log in with their unique details. This robust backend integration provides a
+          reliable and scalable solution for user management, ensuring a personalized experience for
+          each visitor.
           <br></br>
           <br></br>
-          We were given a podcast API and asked to use the data to create a podcast playing App that
-          would fulfil a list of use stories. How we chose to achieve these stories was completely
-          up to our own preference.
+          Beyond user accounts, the store offers practical e-commerce functionalities. Users can add
+          items to their shopping cart or mark them as favorites, with these preferences persisting
+          across sessions. This is achieved through the strategic use of local storage, providing a
+          straightforward and efficient method for storing user-specific data directly within the
+          browser, enhancing the user's shopping journey. The entire interactive experience, from
+          form validation to dynamic content updates, is orchestrated using JavaScript.
           <br></br>
-          <br></br>
-          The project had to be completed with the use of React. Apart from React we had free reign
-          over the technologies we used. I decided to use a Zustand store to help me with the
-          fetching, sorting and storing of the podcast data. The store made it easy to pass the data
-          and functionality to the pages that required it. I also used React Router in the project,
-          making the app into a SPA, and helping to structure the UI and make user navigation
-          easier.
-          <br></br>
-          <br></br>
-          The API we were given had multiple endpoints and it was up to us to navigate these to get
-          the data we required for the app. Once the data was fetched, we had to offer the user way
-          to sort the podcast shows, and to search by title. The user then needed to be able to view
-          a page dedicated to each show, where they could then browse episodes by season. Local
-          Storage was utilised to be able to give the use a way to store their favourites which
-          could then be viewed in a separate favourites page. Local storage was also used to keep
-          track of which episodes the user had already watched and how many times.
+          <br></br>I am continuing to work on this project and will be looking to incorporate the
+          favourites and cart storage to Firebase. I would also like to add functionality whereby
+          the user can add more information to their account details, as well as update or delete
+          their account.
         </p>
       </div>
     </div>
