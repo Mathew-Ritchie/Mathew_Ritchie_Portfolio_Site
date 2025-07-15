03643088ae9d6124104d4b6511bfb59581d0a574
Mathew Ritchie <ritchmat89@gmail.com>
2025-07-15 18:44:16 +0200

    style amendements to projectsHeader components.

--- src/Components/ProjectsHeader.jsx
+++ src/Components/ProjectsHeader.jsx
@@ -8,20 +8,18 @@ import ThemeToggleBtn from "./ThemeToggleBtn";
 export default function ProjectsHeader() {
   return (
     <header className="projects-header-container" id="projects-header-container">
-      <div className="projects-header-div">
-        <img
-          className="projects-header-img"
-          src={newLogo}
-          alt="logo showing computer outline with coding tags on the screen."
-        />
-        <div className="project-header-btn-div">
-          <Link to={"/#project-spacer"} smooth>
-            <button className="projects-header-nav-btn" id="projects-header-nav-btn">
-              Back to projects
-            </button>
-          </Link>
-          <ThemeToggleBtn />
-        </div>
+      <img
+        className="projects-header-img"
+        src={newLogo}
+        alt="logo showing computer outline with coding tags on the screen."
+      />
+      <div className="project-header-btn-div">
+        <Link to={"/#project-spacer"} smooth>
+          <button className="projects-header-nav-btn" id="projects-header-nav-btn">
+            Back to projects
+          </button>
+        </Link>
+        <ThemeToggleBtn />
       </div>
     </header>
   );
