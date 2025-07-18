0518cc06d794dcca330d71e88ab52b72c8a2f12d
Mathew Ritchie <ritchmat89@gmail.com>
2025-07-18 15:47:21 +0200

    cleaned out old project page files. cleaned up App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,10 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainPage from "./pages/Main.jsx";
-
-import FakeStorePage from "./pages/FakeStorePage.jsx";
+import ProjectHeaderFooter from "./Components/ProjectHeaderFooter.jsx";
+import ProjectsPage from "./pages/ProjectsPage.jsx";
 
 import "./App.css";
-import ProjectHeaderFooter from "./Components/ProjectHeaderFooter.jsx";
-
-import RecipeAppPage from "./pages/RecipeAppPage.jsx";
-import ProjectsPage from "./pages/ProjectsPage.jsx";
 
 function App() {
   return (
@@ -18,8 +14,6 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/Project" element={<ProjectHeaderFooter />}>
           <Route path=":projectId" element={<ProjectsPage />} />
-          {/* <Route path="fakeStore" element={<FakeStorePage />} /> */}
-          {/* <Route path="recipe_app" element={<RecipeAppPage />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
